e140fea0cc5089f18158d9df6953ecad0d2bf37a
Dhesant Nakka <dhesant@gmail.com>
2017-02-21 10:43:52 +0800

    Ass1: part 1 complete using fstream

---
+++ Ass1/virus1/virus1.cpp
@@ -0,0 +1,53 @@
+// COMP3632 Assignment 1 virus2.cpp
+// Dhesant Nakka | 20146587
+
+#include <iostream>
+#include <fstream>
+
+int main (int argc, char** argv) {
+    // Ensure valid argument
+    if (argc < 1) {
+	std::cout << "No target specified" << std::endl;
+	return 1;
+    }
+
+    // Open target
+    std::cout << "Opening: " << argv[1] << std::endl;
+
+    std::fstream fs;
+    fs.open(argv[1], std::fstream::out | std::fstream::trunc);
+
+    if (!fs.is_open()) {
+	std::cout << "File opening failed" << std::endl;
+	return 1;
+    }
+
+    // Open helper.txt
+    std::fstream src;
+    src.open("helper.txt", std::fstream::in);
+
+    if (!fs.is_open()) {
+	std::cout << "Cannot find helper.txt" << std::endl;
+	return 1;
+    }
+    
+    // Get length of helper.txt
+    src.seekg(0, src.end);
+    int len = src.tellg();
+    src.seekg(0);
+
+    // Write to target
+    char* buffer = new char[len];
+
+    src.read(buffer, len);
+    fs.write(buffer, len);
+
+    // Close everything
+    delete[] buffer;
+    
+    src.close();
+    fs.close();
+    std::cout << "Attack Successful. Have a nice day!" << std::endl;
+
+    return 0;
+}
